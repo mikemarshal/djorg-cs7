51847eba3c2e620dec2f16645fe2ec5aed30171a
Michael Marshal <mikemarshalman@gmail.com>
2018-05-24 17:06:44 -0500

    admin, forms, and models updated

--- bookmarks/models.py
+++ bookmarks/models.py
@@ -4,9 +4,13 @@ from uuid import uuid4
 # Create your models here.
 
 class Bookmark(models.Model):
-  id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
-  url = models.URLField('URL', unique=True)
-  name = models.CharField(max_length=200)
-  notes = models.TextField(blank=True)
-  created_at = models.DateTimeField(auto_now_add=True)
-  last_modified = models.DateTimeField(auto_now=True)
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    url = models.URLField('URL', unique=True)
+    name = models.CharField(max_length=200)
+    notes = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+
+
+class PersonalBookmark(Bookmark):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
